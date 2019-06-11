b260f3f0cb4156fd927e2a7bc57c247e263817b6
kpilcicki <36018832+kpilcicki@users.noreply.github.com>
2019-06-11 23:41:55 +0200

    Added XML validator and xslt transformer

---
+++ XmlFileModel/Utility/XMLTransformer.cs
@@ -0,0 +1,96 @@
+﻿using Saxon.Api;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlFileModel.Utility
+{
+    public enum XsltOutputType
+    {
+        Txt,
+        Svg,
+        Report
+    }
+
+    public class XMLTransformer
+    {
+        public static string XsltFolderPath = "../../../xml_files";
+
+        public static string TxtXsltFileName = "series_txt.xsl";
+        public static string SvgXsltFileName = "series_svg.xsl";
+        public static string ReportXsltFileName = "series_list.xsl";
+
+
+        public string XmlReportFilePath { get; set; } = "../../../xml_files/series_report.xml";
+
+        public string XmlFilePath { get; set; } = "../../../xml_files/series_list.xml";
+
+        public XMLTransformer()
+        {
+            InitReport();
+        }
+
+        public void InitReport()
+        {
+            Transform(XmlFilePath, XmlReportFilePath, XsltOutputType.Report);
+        }
+
+        public void InitReport(string xmlPath, string reportPath)
+        {
+            Transform(xmlPath, reportPath, XsltOutputType.Report);
+        }
+
+        public void Transform(string outputPath, XsltOutputType outputType = XsltOutputType.Txt)
+        {
+            Transform(XmlReportFilePath, outputPath, outputType);
+        }
+
+        public void Transform(string inputPath, string outputPath, XsltOutputType outputType = XsltOutputType.Txt)
+        {
+            string xsltPath = "";
+            switch (outputType)
+            {
+                case XsltOutputType.Txt:
+                    xsltPath = $"{XsltFolderPath}/{TxtXsltFileName}";
+                    break;
+                case XsltOutputType.Svg:
+                    xsltPath = $"{XsltFolderPath}/{SvgXsltFileName}";
+                    break;
+                case XsltOutputType.Report:
+                    xsltPath = $"{XsltFolderPath}/{ReportXsltFileName}";
+                    break;
+                default:
+                    xsltPath = $"{XsltFolderPath}/{TxtXsltFileName}";
+                    break;
+            }
+
+
+            var processor = new Processor();
+            XsltCompiler compiler = processor.NewXsltCompiler();
+            using (var xsltReader = new StreamReader(xsltPath))
+            {
+                XsltExecutable xsltExecutable = compiler.Compile(xsltReader);
+                XsltTransformer xsltTransformer = xsltExecutable.Load();
+
+                using (var xmlReader = new StreamReader(inputPath))
+                {
+                    xsltTransformer.SetInputStream(xmlReader.BaseStream, new Uri("file://"));
+
+                    using (var sw = new StreamWriter(outputPath))
+                    {
+                        Serializer serializer = processor.NewSerializer(sw);
+
+                        xsltTransformer.Run(serializer);
+
+                    }
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ XmlFileModel/Utility/XMLValidator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XmlFileModel.Utility
+{
+    public class XMLValidator
+    {
+        public static string XmlFilePath = "../../../xml_files/series_list.xml";
+        public static string XsdFilePath = "../../../xml_files/series_list.xsd";
+        public static void Validate()
+        {
+            Validate(XmlFilePath, XsdFilePath);
+        }
+
+        public static void Validate(string xmlPath, string xsdPath)
+        {
+            // Create the XmlSchemaSet class.
+            XmlSchemaSet sc = new XmlSchemaSet();
+
+            // Add the schema to the collection.
+            sc.Add("http://tempuri.org/SeriesPlaylist", xsdPath);
+
+            // Set the validation settings.
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas = sc;
+            settings.ValidationEventHandler += ValidationCallBack;
+
+            // Create the XmlReader object.
+            XmlReader reader = XmlReader.Create(xmlPath, settings);
+
+            // Parse the file. 
+            while (reader.Read()) ;
+
+        }
+
+        // Display any validation errors.
+        private static void ValidationCallBack(object sender, ValidationEventArgs e)
+        {
+            Console.WriteLine($"Validation Error:\n   {e.Message}\n");
+        }
+
+    }
+}
